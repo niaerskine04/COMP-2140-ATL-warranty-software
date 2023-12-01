5738f54dbaaaaf323153537160d055cffa5462fb
niaerskine04 <152611672+niaerskine04@users.noreply.github.com>
2023-12-01 11:17:44 -0500

    Update TicketListing.java

--- 2140 Project 1/src/TicketListing.java
+++ 2140 Project 1/src/TicketListing.java
@@ -1,158 +1,159 @@
 //import java.util.List;
-import java.util.ArrayList;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.Scanner;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 //import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.io.FileWriter;
-
 
 public class TicketListing extends JFrame { // window to display Add ticket option to alt team
-   private JButton cmdAddTicket         = new JButton("Sort By Year");
-   private JButton cmdShowTickets;
-   private JButton cmdClose            = new JButton("X");
+    private JButton cmdAddTicket;
+    private JButton cmdShowTickets;
+    private JButton cmdClose = new JButton("X");
     private JLabel prompt = new JLabel("View customer tickets below");
 
-    //private TicketListing tlisting;        //list of ticket
+    private TicketListing tlisting; // list of ticket
     private ArrayList<Ticket> tlist;
-    
-    //private JFrame tl;
+
+    private JFrame tl;
 
     private DefaultTableModel tablemodel;
 
+    private JPanel pnlCommand = new JPanel();
+    private JPanel pnlDisplay = new JPanel();
 
-    private JPanel pnlCommand           = new JPanel();
-    private JPanel pnlDisplay            = new JPanel();
-public TicketListing(){
-    setVisible(true);
+    public TicketListing() {
+        setVisible(true);
 
-    setSize(400, 300);
+        setSize(400, 300);
 
-    tablemodel = new DefaultTableModel();
-    new GridLayout(2,1);
-    //tlisting = this;
-    tlist = new ArrayList<Ticket>();
-    pnlCommand = new JPanel();
-    pnlDisplay = new JPanel();
-  
-    cmdShowTickets = new JButton("Show Tickets");
-    cmdClose   = new JButton("X");
-    
-    cmdShowTickets.addActionListener(new ShowListener());
-    cmdClose.addActionListener(new CloseListener());
+        tablemodel = new DefaultTableModel();
+        new GridLayout(2, 1);
+        // tlisting = this;
+        tlist = new ArrayList<Ticket>();
+        pnlCommand = new JPanel();
+        pnlDisplay = new JPanel();
+        cmdAddTicket = new JButton("Add New Customer Ticket");
+        cmdShowTickets = new JButton("Show Tickets");
+        cmdClose = new JButton("X");
 
-    pnlDisplay.add(prompt);
+        cmdShowTickets.addActionListener(new ShowListener());
+        cmdClose.addActionListener(new CloseListener());
+        cmdAddTicket.addActionListener(new TicketListener());
 
-    pnlCommand.add(cmdShowTickets);
-    pnlCommand.add(cmdClose);
+        pnlDisplay.add(prompt);
 
-    cmdAddTicket.setBackground(Color.GRAY);
-    cmdAddTicket.setForeground(Color.WHITE);
-    cmdShowTickets.setBackground(Color.GRAY);
-    cmdShowTickets.setForeground(Color.WHITE);
-    cmdClose.setBackground(Color.GRAY);
-    cmdClose.setForeground(Color.PINK);
-    
-   pnlCommand.setPreferredSize(new Dimension(200,100));
-    
-    add(pnlDisplay, BorderLayout.NORTH);
-    add(pnlCommand, BorderLayout.SOUTH);
+        pnlCommand.add(cmdAddTicket);
+        pnlCommand.add(cmdShowTickets);
+        pnlCommand.add(cmdClose);
 
-    setVisible(true);
-    //tl = this;
-    // add details
-}
+        cmdAddTicket.setBackground(Color.GRAY);
+        cmdAddTicket.setForeground(Color.WHITE);
+        cmdShowTickets.setBackground(Color.GRAY);
+        cmdShowTickets.setForeground(Color.WHITE);
+        cmdClose.setBackground(Color.GRAY);
+        cmdClose.setForeground(Color.PINK);
 
-/** 
- * Show Table Method 
- * Shows tickets added to the ticket table
- * @return ArrayList<ticket>
- */
-public void showTable(ArrayList<Ticket> tlist)
-    {
-        for (Ticket t:tlist)
-        {
+        pnlCommand.setPreferredSize(new Dimension(200, 100));
+
+        add(pnlDisplay, BorderLayout.NORTH);
+        add(pnlCommand, BorderLayout.SOUTH);
+
+        setVisible(true);
+        tl = this;
+        // add details
+    }
+
+    /**
+     * Show Table Method
+     * Shows tickets added to the ticket table
+     * 
+     * @return ArrayList<ticket>
+     */
+    public void showTable(ArrayList<Ticket> tlist) {
+        for (Ticket t : tlist) {
             addToTable(t);
         }
     }
 
     /**
-     * Add Ticket Method 
-     * Adds tickets to the ticket table 
+     * Add Ticket Method
+     * Adds tickets to the ticket table
+     * 
      * @param t
      */
-    public void addTicket(Ticket t)
-    {
+    public void addTicket(Ticket t) {
         tlist.add(t);
         addToTable(t);
         saveToFile(t);
     }
 
-        /**
-    Save To File Methood
-     @param Ticket t
-     saves ticket data to file
-    */
-    public void saveToFile(Ticket t){
-        try{
-            FileWriter fw = new FileWriter("SupportTicket.dat",true);
-            fw.write(t.toString()+"\n");
+    /**
+     * Save To File Methood
+     * 
+     * @param Ticket t
+     *               saves ticket data to file
+     */
+    public void saveToFile(Ticket t) {
+        try {
+            FileWriter fw = new FileWriter("SupportTicket.dat", true);
+            fw.write(t.toString() + "\n");
             fw.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
     /**
-     * Add to Table 
-     * adds ticket details to table 
+     * Add to Table
+     * adds ticket details to table
+     * 
      * @param t
      */
-    public void addToTable(Ticket t)
-    {
-        String[] record = {"" +  t.getTicketNumber()+""+t.getTitle()+" "+  t.getName()+" "+t.getContactInfo()+" "+t.getDescription()+ " " + t.getPriority()+ " "+t.getStatus()};
-     tablemodel.addRow(record);        
+    public void addToTable(Ticket t) {
+        String[] record = { "" + t.getTicketNumber() + "" + t.getTitle() + " " + t.getName() + " " + t.getContactInfo()
+                + " " + t.getDescription() + " " + t.getPriority() + " " + t.getStatus() };
+        tablemodel.addRow(record);
     }
 
-        /**
+    /**
      * Load Ticket Methods
-     * loads ticket detaiils from the file so that the ticket can be added to the ticket table 
+     * loads ticket detaiils from the file so that the ticket can be added to the
+     * ticket table
+     * 
      * @param fileName
      * @return
      */
-    public static ArrayList<Ticket> loadTickets(String fileName) 
-    {
+    public static ArrayList<Ticket> loadTickets(String fileName) {
         Scanner scanner = null;
         ArrayList<Ticket> tickets = new ArrayList<>();
-        try 
-        {
+        try {
             scanner = new Scanner(new File(fileName));
-            while (scanner.hasNext()) 
-            {
+            while (scanner.hasNext()) {
                 String line = scanner.nextLine();
                 String[] tokens = line.split(",");
 
-                int ticketNumber  = Integer.parseInt(tokens[0]);
-                String title= tokens[1];
-                String name= tokens[2];
+                int ticketNumber = Integer.parseInt(tokens[0]);
+                String title = tokens[1];
+                String name = tokens[2];
                 String contactInfo = tokens[3];
                 String description = tokens[4];
                 String priority = tokens[5];
-                String status = tokens [6];
-            
-                Ticket ticket = new Ticket(ticketNumber, title, name , contactInfo,description, priority , status);
+                String status = tokens[6];
+
+                Ticket ticket = new Ticket(ticketNumber, title, name, contactInfo, description, priority, status);
                 tickets.add(ticket);
             }
 
@@ -160,32 +161,35 @@ public void showTable(ArrayList<Ticket> tlist)
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-       System.out.println(tickets);
+        System.out.println(tickets);
         return tickets;
     }
 
     // private class TicketListener implements ActionListener
     // {
-    //     public void actionPerformed(ActionEvent c)
-    //     {
-    //         AddTicket addt = new AddTicket(tlisting);  
-    //         System.out.println(addt);          
-    //     }
+    // public void actionPerformed(ActionEvent c)
+    // {
+    // AddTicket addt = new AddTicket(tlisting);
+    // System.out.println(addt);
+    // }
     // }
 
-    private class ShowListener implements ActionListener
-    {
-        public void actionPerformed(ActionEvent sh)
-        {
-           new ShowTicket();
+    private class ShowListener implements ActionListener {
+        public void actionPerformed(ActionEvent sh) {
+            new ShowTicket();
         }
     }
 
-    private class CloseListener implements ActionListener
-    {
-        public void actionPerformed(ActionEvent cl)
-        {
-           tl.setVisible(false);
+    private class CloseListener implements ActionListener {
+        public void actionPerformed(ActionEvent cl) {
+            tl.setVisible(false);
         }
     }
+
+    private class TicketListener implements ActionListener {
+        public void actionPerformed(ActionEvent c) {
+            AddTicket addv = new AddTicket(tlisting);
+            System.out.println(addv);
+        }
     }
+}
